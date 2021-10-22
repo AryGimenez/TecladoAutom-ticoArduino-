245be5eaa734408542909f5dc1e5993003ac7843
Ary <65731115+AryGimenez@users.noreply.github.com>
2021-10-22 17:36:30 -0300

    Sigo Trabajando en el codigo

--- Codigo Fuente/CodigoFuete/CodigoFuete.ino
+++ Codigo Fuente/CodigoFuete/CodigoFuete.ino
@@ -1,11 +1,26 @@
 #include <Keyboard.h>
 const char mConConfiguracion[] PROGMEM = "Preuba";
+const String  mCodConfiguracion = "Constante que guarda la configuración a digitar por teclado"; // 
+int mPocicionCaracter = 0;
+
+const int mLlaveInicio = 3;
+
+boolean mInicioEscrbir= false;
+
 
 void setup() {
   Keyboard.begin();
+  pinMode(mLlaveInicio, INPUT);
 }
 
 void loop() {
+ 
+ 
+ 
+  if (digitalRead(mLlaveInicio) == HIGH){    
+      digitarCaracteres ();
+  
+ 
   Keyboard.press(KEY_RIGHT_GUI);
   Keyboard.press('r');
   delay(100);
@@ -23,6 +38,15 @@ void loop() {
 
 
 void digitarCaracteres (){
-  for int      
+  for (int i = mPocicionCaracter; i < mCodConfiguracion.length(); i ++ ){
+    
+    if (digitalRead(mLlaveInicio) != HIGH)  // Si la llave está apagada termina de disipar por teclado 
+      return;
+    
+    
+    Keyboard.press(mCodConfiguracion.charAt());
+    delay(100);
+    
+  }
 }
 

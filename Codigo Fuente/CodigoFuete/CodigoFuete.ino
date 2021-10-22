abb993f2ddec94705b57b134920e45378f7db5c0
Ary <65731115+AryGimenez@users.noreply.github.com>
2021-10-22 15:48:11 -0300

    Inicie con el con el proyecto. Estoy intentando correr una cadena de caracteres para empezar a mandar señales de teclado por el arduino

---
+++ Codigo Fuente/CodigoFuete/CodigoFuete.ino
@@ -0,0 +1,28 @@
+#include <Keyboard.h>
+const char mConConfiguracion[] PROGMEM = "Preuba";
+
+void setup() {
+  Keyboard.begin();
+}
+
+void loop() {
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('r');
+  delay(100);
+  Keyboard.releaseAll();  
+  delay(1000);
+  
+  Keyboard.println("notepad");
+  
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+  
+  Keyboard.print("Hola mundo!");
+}
+
+
+void digitarCaracteres (){
+  for int      
+}
+
